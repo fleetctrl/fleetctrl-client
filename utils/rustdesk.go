aa86f80c89ccac16b0b3fe34f5e4b1dd1b89a812
Vojtech Kylar <90466277+KiskaLE@users.noreply.github.com>
2025-08-18 11:14:10 +0200

    Replace manual newline trimming with strings.TrimSpace

--- utils/rustdesk.go
+++ utils/rustdesk.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 func GetRustDeskID() (string, error) {
@@ -15,6 +16,6 @@ func GetRustDeskID() (string, error) {
 		return "", fmt.Errorf("chyba při získávání ID: %v", err)
 	}
 	// Trim whitespace and convert to string
-	output = []byte(string(output)[:len(output)-1]) // Remove the trailing newline character
-	return string(output), nil
+	id := strings.TrimSpace(string(output))
+	return id, nil
 }
