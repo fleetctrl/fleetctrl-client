b37e7d8d794eca0a827b2f4788a44a623050a58d
Kylar <vojtech.kylar@usspa.cz>
2025-08-18 11:42:01 +0200

    fix: The `set-output` command is deprecated and will be disabled soon.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	version            = "0.2.1"
+	version            = "0.2.2-test"
 	production         = true
 	serviceName        = "fleetctrl-client"
 	serviceDisplayName = "fleetctrl client"
