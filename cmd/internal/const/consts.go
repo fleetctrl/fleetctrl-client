5604a50fdb31568bfc9797f6ae132e5d955ce5c5
KiskaLE <vojtakylar@seznam.cz>
2025-09-14 18:17:37 +0200

    update version to 0.3.0 in consts

--- cmd/internal/const/consts.go
+++ cmd/internal/const/consts.go
@@ -1,7 +1,7 @@
 package consts
 
 const (
-	Version            = "0.2.2"
+	Version            = "0.3.0"
 	Production         = true
 	ServiceName        = "fleetctrl-client"
 	ServiceDisplayName = "fleetctrl client"
