180cfcf1054a9f22485f549d0956d8cad76d005d
Kylar <vojtech.kylar@usspa.cz>
2025-09-16 07:28:22 +0200

    fix: setting server url to registery

--- cmd/internal/const/consts.go
+++ cmd/internal/const/consts.go
@@ -1,7 +1,7 @@
 package consts
 
 const (
-	Version            = "0.2.2"
+	Version            = "0.2.3"
 	Production         = true
 	ServiceName        = "fleetctrl-client"
 	ServiceDisplayName = "fleetctrl client"
