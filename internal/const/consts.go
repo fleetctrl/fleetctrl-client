b1cdeb6c6a12dd6cfec4c12d3c82ea70d55c8473
KiskaLE <vojtakylar@seznam.cz>
2026-01-08 20:59:53 +0100

    chore: bump version to 0.4.1

--- internal/const/consts.go
+++ internal/const/consts.go
@@ -3,7 +3,7 @@ package consts
 import "time"
 
 const (
-	Version            = "0.4.0"
+	Version            = "0.4.1"
 	Production         = true
 	ServiceName        = "fleetctrl-client"
 	ServiceDisplayName = "fleetctrl client"
