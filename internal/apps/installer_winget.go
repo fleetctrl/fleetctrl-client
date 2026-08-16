cbe9eb95ac2067f714137c7e28da34af98c9a839
Vojtech Kylar <90466277+KiskaLE@users.noreply.github.com>
2026-08-16 14:35:12 +0200

    refactor app management behind injectable manager (#30)
    Split win32 and winget installers behind a common interface, route
    application reconciliation through an injectable application manager,
    and remove unused legacy management loops.

---
+++ internal/apps/installer_winget.go
@@ -0,0 +1,207 @@
+package apps
+
+import (
+	"KiskaLE/RustDesk-ID/internal/models"
+	"KiskaLE/RustDesk-ID/internal/utils"
+	"context"
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+type wingetInstaller struct {
+	ctx       context.Context
+	release   *models.WingetRelease
+	version   string
+	serverUrl string
+}
+
+func (wi *wingetInstaller) Install() error {
+	if wi.release == nil {
+		return fmt.Errorf("winget release data is missing")
+	}
+
+	ctx := wi.ctx
+	version := wi.version
+	wingetID := wi.release.WingetID
+
+	if err := validateWingetID(wingetID); err != nil {
+		return err
+	}
+
+	if err := validateWingetVersion(version); err != nil {
+		return err
+	}
+
+	// Check if a higher version is already installed
+	if version != "" && version != "latest" {
+		installedVersion, err := GetInstalledWingetVersion(wingetID)
+		if err == nil && installedVersion != "" {
+			if CompareVersions(installedVersion, version) > 0 {
+				utils.Infof("Higher version (%s) of %s is already installed (requested version: %s), uninstalling first...", installedVersion, wingetID, version)
+				if err := wi.Uninstall(); err != nil {
+					return fmt.Errorf("failed to uninstall higher version before downgrade: %v", err)
+				}
+			}
+		}
+	}
+
+	utils.Infof("Installing winget app %s (version %s)...", wingetID, version)
+
+	// Wait for any existing winget process to complete
+	waitForWingetLock(30 * time.Minute)
+
+	// Build winget arguments
+	wingetArgs := []string{
+		"install",
+		"--id", wingetID,
+		"--silent",
+		"--force",
+		"--accept-package-agreements",
+		"--accept-source-agreements",
+		"--disable-interactivity",
+	}
+	if version != "" && version != "latest" {
+		wingetArgs = append(wingetArgs, "-v", version)
+	}
+
+	output, err := runWingetCommand(ctx, wingetArgs...)
+	if err != nil {
+		return fmt.Errorf("winget install failed: %v (output: %s)", err, strings.TrimSpace(string(output)))
+	}
+
+	utils.Infof("Successfully installed winget app %s", wingetID)
+
+	return nil
+}
+
+func (wi *wingetInstaller) Uninstall() error {
+	if wi.release == nil {
+		return fmt.Errorf("winget release data is missing")
+	}
+
+	ctx := wi.ctx
+	wingetID := wi.release.WingetID
+	version := wi.version
+
+	if err := validateWingetID(wingetID); err != nil {
+		return err
+	}
+
+	utils.Infof("Uninstalling winget app %s (version %s)...", wingetID, version)
+
+	// Wait for any existing winget process to complete
+	waitForWingetLock(30 * time.Minute)
+
+	output, err := runWingetCommand(
+		ctx,
+		"uninstall",
+		"--id", wingetID,
+		"--silent",
+		"--force",
+		"--accept-source-agreements",
+		"--disable-interactivity",
+	)
+	if err != nil {
+		return fmt.Errorf("winget uninstall failed: %v (output: %s)", err, strings.TrimSpace(string(output)))
+	}
+
+	utils.Infof("Successfully uninstalled winget app %s", wingetID)
+
+	return nil
+}
+
+func (wi *wingetInstaller) IsInstalled() (bool, error) {
+	if wi.release == nil {
+		return false, fmt.Errorf("winget release data is missing")
+	}
+
+	release := wi.release
+	wingetID := release.WingetID
+	version := wi.version
+
+	utils.Infof("Checking winget app %s (version %s)...", wingetID, version)
+
+	installedVersion, err := GetInstalledWingetVersion(wingetID)
+	if err == nil && installedVersion != "" {
+		// If version is specified, we check it
+		if version != "" && version != "latest" {
+			return CompareVersions(installedVersion, version) == 0, nil
+		}
+		// Otherwise just being listed is enough
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (wi *wingetInstaller) Upgrade() error {
+	if wi.release == nil {
+		return fmt.Errorf("winget release data is missing")
+	}
+
+	ctx := wi.ctx
+	wingetID := wi.release.WingetID
+
+	if err := validateWingetID(wingetID); err != nil {
+		return err
+	}
+
+	// Wait for any existing winget process to complete
+	waitForWingetLock(30 * time.Minute)
+
+	output, err := runWingetCommand(
+		ctx,
+		"upgrade",
+		"--id", wingetID,
+		"--silent",
+		"--force",
+		"--accept-package-agreements",
+		"--accept-source-agreements",
+		"--disable-interactivity",
+	)
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			// 0x8a15002b is the exit code for "No applicable update found"
+			if uint32(exitError.ExitCode()) == 0x8a15002b {
+				utils.Info("No applicable update found")
+				return nil
+			}
+		}
+		return fmt.Errorf("winget upgrade failed: %v (output: %s)", err, strings.TrimSpace(string(output)))
+	}
+
+	return nil
+}
+
+// waitForWingetLock waits for any existing winget process to complete.
+// If the process doesn't complete within the timeout, it will be killed.
+func waitForWingetLock(timeout time.Duration) {
+	checkScript := `Get-Process -Name winget -ErrorAction SilentlyContinue | Select-Object -First 1`
+	killScript := `Get-Process -Name winget -ErrorAction SilentlyContinue | Stop-Process -Force`
+
+	startTime := time.Now()
+	for {
+		// Check if winget is running
+		cmd := exec.Command("powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command", checkScript)
+		output, _ := cmd.Output()
+
+		if len(strings.TrimSpace(string(output))) == 0 {
+			// No winget process running
+			return
+		}
+
+		// Check timeout
+		if time.Since(startTime) >= timeout {
+			utils.Info("Winget process didn't complete in time, killing it...")
+			killCmd := exec.Command("powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command", killScript)
+			killCmd.Run()
+			time.Sleep(1 * time.Second) // Wait a bit for the process to be killed
+			return
+		}
+
+		utils.Info("Waiting for another winget process to complete...")
+		time.Sleep(2 * time.Second)
+	}
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -3,9 +3,9 @@ package service
 import (
 	"KiskaLE/RustDesk-ID/internal/apps"
 	"KiskaLE/RustDesk-ID/internal/auth"
-	"KiskaLE/RustDesk-ID/internal/database"
 	"KiskaLE/RustDesk-ID/internal/models"
 	"KiskaLE/RustDesk-ID/internal/utils"
+	"context"
 	"encoding/json"
 	"log"
 	"os/exec"
@@ -13,96 +13,27 @@ import (
 	"time"
 )
 
+type ApplicationManager interface {
+	IsInstalled(ctx context.Context, release models.AssignedRelease, serverURL string) (bool, error)
+	Install(ctx context.Context, release models.AssignedRelease, serverURL string) error
+	Uninstall(ctx context.Context, release models.AssignedRelease, serverURL string) error
+	SupportsUpgrade(ctx context.Context, release models.AssignedRelease, serverURL string) bool
+	Upgrade(ctx context.Context, release models.AssignedRelease, serverURL string) error
+}
+
 type MainService struct {
 	as        *auth.AuthService
 	serverURL string
 	Tokens    *auth.Tokens
+	apps      ApplicationManager
 }
 
-func NewMainService(as *auth.AuthService, serverURL string) *MainService {
-	return &MainService{as: as, serverURL: serverURL}
-}
-
-func (ms *MainService) legacyStartRustDeskServerSync() {
-	utils.Info("Starting RustDesk sync...")
-	for {
-		// get rustdesk ID
-		rustdeskID, err := utils.GetRustDeskID()
-		if err != nil {
-			utils.Error(err)
-		}
-		// get PC name
-		computerName, err := utils.GetComputerName()
-		if err != nil {
-			utils.Error(err)
-			time.Sleep(15 * time.Minute)
-			continue
-		}
-		// get PC IP
-		computerIP, err := utils.GetComputerIP()
-		if err != nil {
-			utils.Error(err)
-		}
-		// get OS
-		osName, err := utils.GetComputerOS()
-		if err != nil {
-			utils.Error(err)
-		}
-		// get OS version
-		osVersion, err := utils.GetComputerOSVersion()
-		if err != nil {
-			utils.Error(err)
-		}
-
-		loginUser, err := utils.GetCurrentUser()
-		if err != nil {
-			utils.Error(err)
-		}
-
-		intuneID, err := utils.GetIntuneID()
-		if err != nil {
-			utils.Error(err)
-		}
-
-		computer := models.Computer{
-			Name:           computerName,
-			RustdeskID:     rustdeskID,
-			IP:             computerIP,
-			OS:             osName,
-			OSVersion:      osVersion,
-			LoginUser:      loginUser,
-			IntuneID:       intuneID,
-			LastConnection: time.Now().Format(time.RFC3339),
-		}
-
-		res, err := utils.Patch(ms.serverURL+"/computer/rustdesk-sync", map[string]string{
-			"name":            computer.Name,
-			"rustdesk_id":     computer.RustdeskID,
-			"ip":              computer.IP,
-			"os":              computer.OS,
-			"os_version":      computer.OSVersion,
-			"login_user":      computer.LoginUser,
-			"intune_id":       computer.IntuneID,
-			"last_connection": computer.LastConnection,
-		}, map[string]string{
-			"Content-Type": "application/json",
-		})
-		if err != nil {
-			log.Println(err)
-			time.Sleep(15 * time.Minute)
-			continue
-		}
-
-		if res.StatusCode != 200 {
-			// parse body
-			utils.Error("Server returned status code: ", utils.ParseHttpError(res))
-			res.Body.Close()
-			time.Sleep(15 * time.Minute)
-			continue
-		}
-		res.Body.Close()
-		time.Sleep(5 * time.Minute)
+func NewMainService(as *auth.AuthService, serverURL string, appManager ApplicationManager) *MainService {
+	if appManager == nil {
+		appManager = apps.NewManager()
 	}
+
+	return &MainService{as: as, serverURL: serverURL, apps: appManager}
 }
 
 func (ms *MainService) StartRustDeskServerTasks() {
@@ -254,235 +185,6 @@ func (ms *MainService) reportReleaseInstallState(releaseID string, status apps.R
 	if err := apps.ReportReleaseInstallState(ms.serverURL, releaseID, status, installedAt, &lastSeenAt); err != nil {
 		utils.Errorf("Failed to report install state %s for release %s: %v", status, releaseID, err)
 	}
-}
-
-func (ms *MainService) legacyStartApplicationsManagement() {
-	utils.Info("Starting applications management...")
-
-	for {
-		// get asigned applications
-		appsResponse, err := utils.Get(ms.serverURL+"/apps/assigned", map[string]string{
-			"Content-Type": "application/json",
-		})
-		if err != nil {
-			log.Println(err)
-			time.Sleep(15 * time.Minute)
-			continue
-		}
-		if appsResponse.StatusCode != 200 {
-			// parse body
-			utils.Error("Server returned error: ", appsResponse.StatusCode)
-			appsResponse.Body.Close()
-			time.Sleep(15 * time.Minute)
-			continue
-		}
-
-		var assignedAppsResponse models.AssignedAppsResponse
-		if err := json.NewDecoder(appsResponse.Body).Decode(&assignedAppsResponse); err != nil {
-			utils.Error(err)
-			appsResponse.Body.Close()
-			time.Sleep(15 * time.Minute)
-			continue
-		}
-		appsResponse.Body.Close()
-
-		if len(assignedAppsResponse.Apps) == 0 {
-			utils.Info("No assigned apps found")
-			time.Sleep(15 * time.Minute)
-			continue
-		}
-
-		for _, app := range assignedAppsResponse.Apps {
-			utils.Info("Processing app: ", app.DisplayName)
-			if len(app.Releases) == 0 {
-				utils.Errorf("App %s has no releases, skipping", app.DisplayName)
-				continue
-			}
-			newestRelease := app.Releases[len(app.Releases)-1]
-			if newestRelease.AssignType == "exclude" {
-				continue
-			}
-
-			switch newestRelease.Action {
-			case "install":
-				// check if application is installed
-				installed, err := apps.IsAppInstalled(newestRelease)
-				if err != nil {
-					utils.Errorf("Failed to check if app is installed: %v", err)
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateError, nil)
-					continue
-				}
-
-				if installed {
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateInstalled, nil)
-					if newestRelease.InstallerType == "winget" && newestRelease.Winget != nil && app.AutoUpdate {
-						// Check if we should check for updates (once per 24h)
-						shouldCheck, err := database.ShouldCheckWinget(newestRelease.Winget.WingetID)
-						if err != nil {
-							utils.Errorf("Failed to check database for winget check time: %v", err)
-							shouldCheck = true // Check anyway on error
-						}
-
-						if shouldCheck {
-							utils.Infof("Checking for updates for winget app %s...", newestRelease.Winget.WingetID)
-							if err := apps.UpgradeApp(newestRelease); err != nil {
-								utils.Errorf("Failed to upgrade winget app %s: %v", newestRelease.Winget.WingetID, err)
-							}
-							// Mark as checked
-							if err := database.UpdateWingetCheck(newestRelease.Winget.WingetID); err != nil {
-								utils.Errorf("Failed to update winget check time in database: %v", err)
-							}
-						} else {
-							utils.Infof("Skipping winget update check for %s (already checked in last 24h)", app.DisplayName)
-						}
-					}
-					continue
-				}
-
-				// Check backoff for install
-				shouldAttempt, err := database.ShouldAttemptApp(newestRelease.ID)
-				if err != nil {
-					utils.Errorf("Failed to check backoff: %v", err)
-					shouldAttempt = true
-				}
-				if !shouldAttempt {
-					utils.Infof("Skipping installation of %s due to backoff after multiple failures", app.DisplayName)
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateError, nil)
-					continue
-				}
-
-				ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateInstalling, nil)
-
-				// application is not installed
-				// install application
-				if newestRelease.UninstallPrevious {
-					// uninstall previous versions
-					for _, release := range app.Releases {
-						if release.Version == newestRelease.Version {
-							continue
-						}
-
-						// Check if this previous version is actually installed before uninstalling
-						prevInstalled, prevErr := apps.IsAppInstalled(release)
-						if prevErr != nil {
-							utils.Errorf("Failed to check if previous version %s is installed: %v", release.Version, prevErr)
-							continue
-						}
-
-						if prevInstalled {
-							utils.Info("Previous version is installed, uninstalling...")
-
-							// Check backoff for uninstall
-							shouldAttempt, err := database.ShouldAttemptApp(release.ID)
-							if err != nil {
-								utils.Errorf("Failed to check backoff: %v", err)
-								shouldAttempt = true
-							}
-							if !shouldAttempt {
-								utils.Infof("Skipping uninstallation of previous version %s due to backoff after multiple failures", release.Version)
-								continue
-							}
-
-							if err := apps.UninstallApp(release, ms.serverURL); err != nil {
-								utils.Errorf("Failed to uninstall previous version: %v", err)
-								database.RecordAppFailure(release.ID)
-							} else {
-								database.ResetAppFailures(release.ID)
-							}
-							break
-						}
-					}
-				}
-
-				err = apps.InstallApp(newestRelease, ms.serverURL)
-				if err != nil {
-					utils.Errorf("Failed to install app: %v", err)
-					database.RecordAppFailure(newestRelease.ID)
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateError, nil)
-				} else {
-					database.ResetAppFailures(newestRelease.ID)
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateInstalled, nowUnixMilliPtr())
-				}
-
-				// check if app is installed
-				installed, err = apps.IsAppInstalled(newestRelease)
-				if err != nil {
-					utils.Errorf("Failed to check if app is installed: %v", err)
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateError, nil)
-					continue
-				}
-				if !installed {
-					utils.Errorf("Unable to install application")
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateError, nil)
-					continue
-				}
-
-			case "uninstall":
-				// check if application is uninstalled
-				installed, err := apps.IsAppInstalled(newestRelease)
-				if err != nil {
-					log.Println(err)
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateError, nil)
-					continue
-				}
-				if !installed {
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateUninstalled, nil)
-					continue
-				}
-
-				// application is installed — try to uninstall each release version that is present
-				for _, release := range app.Releases {
-					releaseInstalled, relErr := apps.IsAppInstalled(release)
-					if relErr != nil {
-						utils.Errorf("Failed to check if release %s is installed: %v", release.Version, relErr)
-						continue
-					}
-					if !releaseInstalled {
-						continue
-					}
-
-					utils.Infof("Version %s is installed, uninstalling...", release.Version)
-
-					// Check backoff
-					shouldAttempt, err := database.ShouldAttemptApp(release.ID)
-					if err != nil {
-						utils.Errorf("Failed to check backoff: %v", err)
-						shouldAttempt = true
-					}
-					if !shouldAttempt {
-						utils.Infof("Skipping uninstallation of %s due to backoff after multiple failures", app.DisplayName)
-						continue
-					}
-
-					if err := apps.UninstallApp(release, ms.serverURL); err != nil {
-						log.Printf("Failed to uninstall version %s: %v", release.Version, err)
-						database.RecordAppFailure(release.ID)
-						ms.reportReleaseInstallState(release.ID, apps.ReleaseInstallStateError, nil)
-					} else {
-						database.ResetAppFailures(release.ID)
-						ms.reportReleaseInstallState(release.ID, apps.ReleaseInstallStateUninstalled, nil)
-					}
-					break
-				}
-
-				// check if application is uninstalled
-				installed, err = apps.IsAppInstalled(newestRelease)
-				if err != nil {
-					utils.Errorf("Failed to check if app is installed: %v", err)
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateError, nil)
-					continue
-				}
-				if installed {
-					utils.Errorf("Unable to uninstall application")
-					ms.reportReleaseInstallState(newestRelease.ID, apps.ReleaseInstallStateError, nil)
-					continue
-				}
-			}
-		}
-
-		time.Sleep(15 * time.Minute)
-	}
-
 }
 
 func (ms *MainService) GetAuthService() *auth.AuthService {
